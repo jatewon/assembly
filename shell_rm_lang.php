6f1ec4c695d685f439b8899b5fdf2ea67569ee65
jatewon <84976508+jatewon@users.noreply.github.com>
2022-07-09 09:25:49 +0700

    Update shell_rm_lang.php

--- shell_rm_lang.php
+++ shell_rm_lang.php
@@ -1,10 +1,1 @@
-<html>
-    <body>
-        <pre>
-            <?php $myfile = fopen("index.php", "r") or die("Unable to open file!");
-            echo fread($myfile,filesize("index.php"));
-            fclose($myfile);
-            ?>
-        </pre>
-    </body>
-</html>
+<?php echo file_get_contents("index.php";)?>
